b0e739a792514677f6a766a1b4d0a873632a5d44
Vaishnavi Jadhav <vaishnavijadhav@Vaishnavis-MacBook-Pro.local>
2026-01-17 23:08:44 -0600

    Initial commit: release the chaos

---
+++ test_script.py
@@ -0,0 +1,19 @@
+# Delulify Test Script
+
+# Uncomment each block below one by one to test different exceptions.
+
+# 1. ZeroDivisionError
+# print(1 / 0)
+
+# 2. IndexError
+# lst = [1, 2, 3]
+# print(lst[5])
+
+# 3. NameError
+# print(undefined_variable)
+
+# 4. SyntaxError (Note: This causes the entire script to fail to run)
+# def unfinished_function(
+
+# 5. Unhandled Exception
+raise ValueError("This is an unhandled ValueError for testing.")
